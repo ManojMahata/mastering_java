0969ae9c310030a6143e0ac6366ec29ec9bb888c
ManojMahata <mahatamanoj402@gmail.com>
2026-01-28 17:04:41 +0545

    program to calculate are of given raidus

---
+++ Month1Learning/basics/week1/areOfCirlce.java
@@ -0,0 +1,15 @@
+public class areOfCirlce{
+
+    public static void main(String[] args){
+
+        // pi R square is the formula (\(Area=\pi r^{2}\)
+
+        double pi = 3.14159;
+        double r = 5;
+
+        double area = pi * r * r;
+
+        System.out.println("Area of given radius is: "+ area);
+    }
+
+}
