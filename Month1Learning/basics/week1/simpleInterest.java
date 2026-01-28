64dda9344d6663e1f4c7295245ac5bdf83012f8e
ManojMahata <mahatamanoj402@gmail.com>
2026-01-28 18:11:02 +0545

    program to find simple interes

---
+++ Month1Learning/basics/week1/simpleInterest.java
@@ -0,0 +1,17 @@
+public class simpleInterest {
+
+	public static void main(String[] args){
+
+		// Simple interest 
+		// principle p, time t, and rate r
+		// interest i = (p*t*r)/100;
+
+		double p = 100000;
+		double t = 12;
+		double rate = 2;
+		double i = (p*t*rate)/100;
+
+		System.out.println("Simple interest: " + i);
+
+	}
+}
