faa48e0f5fd01579c491b7acc0f7b7ab9893fa75
ManojMahata <mahatamanoj402@gmail.com>
2026-01-28 17:09:12 +0545

    learnd about data types

---
+++ Month1Learning/basics/week1/dayone2.java
@@ -0,0 +1,10 @@
+public class dayone2 {
+
+	public static void main(String[] args){
+
+		System.out.println("Name: Manoj");
+		System.out.println("College: Greenfield");
+		System.out.println("Address: Sitapaila");
+
+	}
+}
