60e89a014f62559919efbdd2e920264aa2fdcf03
ManojMahata <mahatamanoj402@gmail.com>
2026-01-28 17:10:32 +0545

    day two

---
+++ Month1Learning/basics/week1/daytwo1.java
@@ -0,0 +1,29 @@
+public class daytwo1{
+
+	public static void main(String[] args){
+
+	//learning about data types in Java
+	/* * Primitive data types (`int`, `double`, `char`, `boolean`)
+	* Variable declaration & initialization
+	* Type casting (basic)
+	**Code practice:**
+	* Store and print student details
+	* Calculate:
+	* Area of circle
+	* Simple interest*/
+
+	int a = 22;
+	double gpa = 3.23;
+	char grade = 'A';
+	boolean isPassing = true;
+	
+	System.out.println("Age: "+ a);
+	System.out.println("Gpa: "+ gpa);
+	System.out.println("Grade: "+ grade);
+	System.out.println("Is passing the BCA in time: "+ isPassing);
+
+	
+
+
+	}
+}
