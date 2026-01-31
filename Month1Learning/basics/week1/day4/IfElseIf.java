0ac164e15293814029a1cf7ec9b4db24b60198ca
ManojMahata <mahatamanoj402@gmail.com>
2026-01-31 16:40:41 +0545

    learned about if else statements

---
+++ Month1Learning/basics/week1/day4/IfElseIf.java
@@ -0,0 +1,14 @@
+public class IfElseIf {
+    public static void main(String[] args){
+
+        System.out.println("If else if statement.");
+
+        int a = 1;
+        if (a == 1)
+            System.out.println("Hello.");
+        else System.out.println("No Hello.");
+
+        
+    }
+    
+}
