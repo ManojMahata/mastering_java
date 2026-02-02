a1244d9c8170ee27ccf4857728474f3fde8a0e13
ManojMahata <mahatamanoj402@gmail.com>
2026-02-02 17:44:37 +0545

    Arthimetic Operators

---
+++ Month1Learning/basics/Variables/ArthimeticOperator.java
@@ -0,0 +1,14 @@
+public class ArthimeticOperator{
+    public static void main (String[] args){
+
+        float a = 10f;
+        float b = 3;
+        float sum = a + b;
+        float diff = a - b;
+        float product = a * b;
+        float quotient = a/b;
+
+        System.out.println("Addition: " + sum + "\n"+"Difference: "+ diff + "\n" + "Product: " + product + "\n" + "Division: " + quotient + "\n");
+
+    }
+}
