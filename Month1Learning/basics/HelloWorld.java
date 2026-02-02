7e1333ae7783df3374e7431f6221b3e747b37e45
ManojMahata <mahatamanoj402@gmail.com>
2026-02-02 17:08:34 +0545

    Hello World program.

---
+++ Month1Learning/basics/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.print("hello world.");
+        // I think this is my 3rd or 4th thime learning Java from basic
+        // even though I did syntx error.j
+
+    }
+}
