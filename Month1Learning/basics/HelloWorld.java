74ff23184aca84beb1322419421e42fdb24e3824
ManojMahata <mahatamanoj402@gmail.com>
2026-02-02 17:24:00 +0545

    h

--- Month1Learning/basics/HelloWorld.java
+++ Month1Learning/basics/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
     public static void main(String[] args){
         System.out.print("hello world.");
         // I think this is my 3rd or 4th thime learning Java from basic
-        // even though I did syntx error.j
+        // even though I did syntx error.
 
     }
 }
