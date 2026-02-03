f6b471b2322cacd6d3858af7c8643d0c125cd31e
ManojMahata <mahatamanoj402@gmail.com>
2026-02-03 17:53:22 +0545

    ComparisionOperator

---
+++ Month1Learning/basics/Variables/ComparisionOperator.java
@@ -0,0 +1,22 @@
+// Variables in Java has three components,
+// Data type, Variable Name, and Value.
+
+public class ComparisionOperator{
+    public static void main(String[] args){
+        //declaring and initializing variables;
+
+        //integer variable
+        int a = 21;
+
+        //string variable
+        String name = "Manoj Mahat";
+        
+        double salary = 400000.99;
+
+        //display the values of variables
+        System.out.println("Age: "+a);
+        System.out.println("Name: "+name);
+        System.out.println("Salary: "+salary);
+
+    }
+}
