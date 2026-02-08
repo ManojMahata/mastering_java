232cb382fc1dfab3075152bac3d70898918d23b8
ManojMahata <mahatamanoj402@gmail.com>
2026-02-08 22:17:07 +0545

    type casting

---
+++ Month1Learning/basics/TypeCasting/ProblemTwo.java
@@ -0,0 +1,13 @@
+public class ProblemTwo {
+
+    public static void main(String[] args) {
+        
+        double x = 9.7;
+
+        double y = (double) x + 2;
+
+        System.out.println(y);
+
+
+    }
+}
