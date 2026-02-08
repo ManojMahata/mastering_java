99cdc9e1fe0d2cbb106a0a45b1b4fc2de7246461
ManojMahata <mahatamanoj402@gmail.com>
2026-02-08 22:04:27 +0545

    problem one

---
+++ Month1Learning/basics/TypeCasting/ProblemOne.java
@@ -0,0 +1,23 @@
+public class ProblemOne {
+
+    public static void main(String[] args) {
+        
+        double result = 7/2;
+        System.out.println(result);
+
+        // correct for result is :
+        /*
+
+            double result = 7/2.0;
+            or 
+            double resutl = (double) 7/2.0;
+
+        */
+        
+
+        double re = (double) 6.4/2;
+        System.out.println(re);
+
+
+    }
+}
