ec6ce382aedc290f14a996329030e0576640eb71
ManojMahata <mahatamanoj402@gmail.com>
2026-02-09 18:15:42 +0545

    if in java

---
+++ Month1Learning/basics/ControlFlow/conditionalStatements/If.java
@@ -0,0 +1,20 @@
+import java.util.*;
+// if in java: 
+
+public class If {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // let's see how if statement works
+
+        System.out.print("Enter Integer number : ");
+        int number = scan.nextInt();
+
+        if (number > 20) {
+            System.out.println("So the number is greater then 20.");
+        }
+        
+
+
+    }
+}
