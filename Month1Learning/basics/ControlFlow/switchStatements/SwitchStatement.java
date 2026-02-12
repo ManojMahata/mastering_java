e4ba1bce4eeea9779286bc872d1ce04722a9647b
ManojMahata <mahatamanoj402@gmail.com>
2026-02-12 12:58:32 +0545

    learning switch statements:

---
+++ Month1Learning/basics/ControlFlow/switchStatements/SwitchStatement.java
@@ -0,0 +1,9 @@
+public class SwitchStatement {
+
+    public static void main(String[] args) {
+        /* switch statement is used when we have to compare one variable against multiple fixed values and keep code cleaner then usign many else if */
+
+        System.out.println("Switch statement dosen's supports float, double, and boolean");
+
+    }
+}
