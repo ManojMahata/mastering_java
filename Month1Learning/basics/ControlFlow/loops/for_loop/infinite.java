80cbb64a86b7c9e92825910c2e9bb8bef54e4ac8
ManojMahata <mahatamanoj402@gmail.com>
2026-02-13 21:50:30 +0545

    comment out this shit

--- Month1Learning/basics/ControlFlow/loops/for_loop/infinite.java
+++ Month1Learning/basics/ControlFlow/loops/for_loop/infinite.java
@@ -2,9 +2,9 @@ public class infinite {
 
     public static void main(String[] args) {
 
-        for (; ; ) {
-            System.out.println("infinite");
-        }
+        // for (; ; ) {
+        //     System.out.println("infinite");
+        // }
 
             
         }
