b4834cc567da94a872c94687daf9c1c3a9597ef5
ManojMahata <mahatamanoj402@gmail.com>
2026-02-13 21:47:28 +0545

    step increment in for loop

---
+++ Month1Learning/basics/ControlFlow/loops/for_loop/stepIncrement.java
@@ -0,0 +1,16 @@
+public class stepIncrement {
+
+    public static void main(String[] args) {
+        
+        for (int i = 0; i <= 10; i +=2){
+            System.out.println(i);
+        }
+
+        for (int a = 0; a <= 10; a += 2 ) {
+            System.out.println(a);
+        }
+
+        
+
+    }
+}
