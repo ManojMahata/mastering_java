91be1a344d9ef88702a7ede4de6faef8e63bb4bf
ManojMahata <mahatamanoj402@gmail.com>
2026-02-13 21:42:19 +0545

    reverse for loop example

---
+++ Month1Learning/basics/ControlFlow/loops/for_loop/reverseForloopExample.java
@@ -0,0 +1,14 @@
+public class reverseForloopExample {
+
+    public static void main(String[] args) {
+        
+
+        // for (int i = 5; i >=1 ; i-- ) {
+        //     System.out.println(i);
+        // }
+
+        for (int a = 12; a >=0 ; a-- ) {
+            System.out.println(a);
+        }
+    }
+}
