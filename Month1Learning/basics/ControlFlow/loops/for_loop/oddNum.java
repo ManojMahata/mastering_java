7b1a78dad413f2fdc01d5b7559e1993d54780681
ManojMahata <mahatamanoj402@gmail.com>
2026-02-15 20:19:38 +0545

    odd number using for loop

---
+++ Month1Learning/basics/ControlFlow/loops/for_loop/oddNum.java
@@ -0,0 +1,14 @@
+public class oddNum {
+
+    public static void main(String[] args) {
+        
+        // print odd number from upto 100
+
+        for (int i=0; i <=100; i++) {
+            if (i %2 == 1) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
