f971b736821fd94114345cc7514b3342f685658b
ManojMahata <mahatamanoj402@gmail.com>
2026-02-18 22:15:46 +0545

    program to return sum of given numbers

---
+++ Month1Learning/basics/ControlFlow/loops/while_loop/problems/SumofDigit.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumofDigit {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter number : ");
+        int num = scan.nextInt();
+
+        int sum = 0;
+
+        while (num != 0) {
+            
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println(sum);
+
+
+        scan.close();
+    }
+}
