33d973e147d70a2f69cdcfff5c2ddc081cac0c9e
ManojMahata <mahatamanoj402@gmail.com>
2026-02-18 18:11:56 +0545

    started while loop

---
+++ Month1Learning/basics/ControlFlow/loops/while_loop/problems/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+
+    public static void main(String[] args) {
+        
+        int i = 54;
+        while (i <= 57) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
