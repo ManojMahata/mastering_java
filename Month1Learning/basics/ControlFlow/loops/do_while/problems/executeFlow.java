2ca21e501b66ad420bfd7e5d793bf7ba7a75ad4e
ManojMahata <mahatamanoj402@gmail.com>
2026-02-20 20:03:55 +0545

    started do while

---
+++ Month1Learning/basics/ControlFlow/loops/do_while/problems/executeFlow.java
@@ -0,0 +1,19 @@
+ public class executeFlow {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        do {
+            System.out.println("Hello");
+        } while (a < 3);
+
+        int b = 3;
+
+        do {
+            System.out.println(b);
+            b++;
+        } while ( b >= 5);
+
+    }
+}
