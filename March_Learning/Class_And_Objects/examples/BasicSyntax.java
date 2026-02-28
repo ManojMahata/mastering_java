60bee0fb09a706887c17f74f063c14014cc47268
ManojMahata <mahatamanoj402@gmail.com>
2026-02-28 23:46:35 +0545

    a

---
+++ March_Learning/Class_And_Objects/examples/BasicSyntax.java
@@ -0,0 +1,19 @@
+class Student {
+	String name;
+	int age;
+
+	void display(){
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+	}
+}
+
+public class BasicSyntax{
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.name = "Ram";
+		s1.age = 21;
+
+		s1.display();
+	}
+}
