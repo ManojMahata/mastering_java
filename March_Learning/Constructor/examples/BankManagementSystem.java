ddd36c35b94b5e3c5b2a2d70e0a1b52aeb5ec851
ManojMahata <mahatamanoj402@gmail.com>
2026-03-02 15:22:19 +0545

    learning constructor

---
+++ March_Learning/Constructor/examples/BankManagementSystem.java
@@ -0,0 +1,24 @@
+class bankClass {
+	// created class and attributes
+	int accountNumber;
+	int balance;
+	// constructor
+	bankClass( int aN, int bLn){
+		accountNumber = aN;
+		balance = bLn;
+	}
+	// method to display info stored in our constructor
+	void displayInfo(){
+		System.out.println("Account Number: " + accountNumber);
+		System.out.println("Balance: " + balance);
+	}
+}
+
+public class BankManagementSystem {
+
+	public static void main(String[] args) {
+		// object wiht constructor values
+		bankClass b1 = new bankClass(1122, 1999);
+		b1.displayInfo();
+	}
+}
