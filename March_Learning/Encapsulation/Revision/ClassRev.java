caaf48ee03ddd6939846108e81a06cf76ea7117d
ManojMahata <mahatamanoj402@gmail.com>
2026-03-11 14:11:56 +0545

    constructor and object

--- March_Learning/Encapsulation/Revision/ClassRev.java
+++ March_Learning/Encapsulation/Revision/ClassRev.java
@@ -3,6 +3,12 @@ class Student {
 	String name = "";
 	int age;
 
+	// constructor
+	Student (String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+
 	// behavior or method
 	void showInfo(){
 		System.out.println("My name is " + name + " and I'm " + age + " year old.");
@@ -12,7 +18,8 @@ class Student {
 public class ClassRev {
 
 	public static void main(String[] args) {
-		Student s = new Student();
+		Student s = new Student(); // object is created to run/initialize values in class/blueprint.
+
 		s.showInfo();
 	}
 }
--- March_Learning/Encapsulation/Revision/ObjectRev.java
+++ March_Learning/Encapsulation/Revision/ObjectRev.java
@@ -1,9 +1,11 @@
 public class ObjectRev {
 
 	public static void main(String[] args) {
-		Student s1  = new Student();
-		s1.name = "Ram";
-		s1.age = 22;
+		Student s1  = new Student("Manoj Mahata", 21);
+		// created object and initialized values using constructor.
+
+		// s1.name = "Ram";
+		// s1.age = 22;
 		s1.showInfo();
 	}
 }
