c9c1920eaa7b05da5f0e6200c2f83f48e9e1bd9d
ManojMahata <mahatamanoj402@gmail.com>
2026-03-22 17:51:54 +0545

    Problem 2

---
+++ March_Learning/Interfaces/practice/p2.java
@@ -0,0 +1,19 @@
+/* 2. Write a Java program to create a Animal interface with a method called bark() that takes no arguments and returns void. Create a Dog class that implements Animal and overrides speak() to print "Dog is barking". */
+
+interface Animal{
+	void bark();
+}
+
+class Dog implements Animal{
+	public void bark(){
+		System.out.println("Dog is barking..");
+	}
+}
+
+public class p2 {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		dog.bark();
+	}
+}
