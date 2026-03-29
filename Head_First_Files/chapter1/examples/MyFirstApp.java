f98680b8ba6a558db2428c596ff20a0472845122
ManojMahata <mahatamanoj402@gmail.com>
2026-03-29 15:22:52 +0545

    m

---
+++ Head_First_Files/chapter1/examples/MyFirstApp.java
@@ -0,0 +1,14 @@
+public class MyFirstApp {
+
+	public static void main(String[] args) {
+		System.out.print("I'll conqueror ");
+		System.out.println("Java.");
+
+		int x = 3;
+		if (x == 3){
+			System.out.println("X must be 3");
+		}
+		System.out.println("This runs no matter what");
+
+	}
+}
---
+++ Head_First_Files/chapter1/examples/Party.java
@@ -0,0 +1,21 @@
+import java.awt.*;	
+import java.awt.event.*;
+
+class P {
+public void buildInvite(){
+        Frame f = new Frame();
+        Label l = new Label("Party at Tim's ");
+        Button b = new Button("you bet");
+        Button c = new Button("Shoot me");
+        Panel p = new Panel();
+        p.add(l); 
+    }
+}
+
+public class Party{
+    public static void main(){
+            P p = new P();
+                p.buildInvite();
+    }
+}   
+// I'll complete this one leter
